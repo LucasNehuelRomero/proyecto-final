3666d1da3a50b2d217a4f10c4e40838fb9277fc6
Lucas Romero <romerolucas561@gmail.com>
2021-11-07 11:01:22 -0300

    css

--- src/componentes/Sidebar.js
+++ src/componentes/Sidebar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../estilos/Sidebar.css';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
         <div className="Sidebar-body">
             <h1>Sidebar</h1>
+       <SidebarOption title="Home" />
+       <SidebarOption title="Search" />
+       <SidebarOption title="Library" />
+       
+       
         </div>
+
     )
 }
 
